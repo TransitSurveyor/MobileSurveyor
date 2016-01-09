d06fb18cfc66b7ae0b36ebfc8b8858a3faaaa857
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-09 12:15:44 -0800

    Add some basic tests to verify QuestionActivity functionality

---
+++ app/src/androidTest/java/com/meyersj/mobilesurveyor/app/questions/questions/QuestionActivityTestBase.java
@@ -0,0 +1,32 @@
+package com.meyersj.mobilesurveyor.app.questions.questions;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v4.app.Fragment;
+
+import com.meyersj.mobilesurveyor.app.survey.QuestionActivity;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+
+
+public abstract class QuestionActivityTestBase {
+
+    public final String TAG = getClass().getCanonicalName();
+    public String action = QuestionActivity.GEO_LOCATION_ACTION;
+    public QuestionActivity activity;
+
+    @Rule
+    public ActivityTestRule<QuestionActivity> activityTestRule = new ActivityTestRule<QuestionActivity>(QuestionActivity.class);
+
+    public Fragment initializeActivity() {
+        Intent intent = new Intent(action);
+        activity = activityTestRule.launchActivity(intent);
+        Assert.assertEquals("Activity action not set", activity.getAction(), action);
+        activity = activityTestRule.launchActivity(new Intent(action));
+        Fragment fragment = activity.getFragment();
+        Assert.assertNotNull("Fragment is null", fragment);
+        return fragment;
+    }
+}
