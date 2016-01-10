9a97a553349f23358384eb6d5aa1d25e00e5b39e
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-09 17:18:44 -0800

    Add keys for ODK fields

---
+++ app/src/main/java/com/meyersj/mobilesurveyor/app/Fields.java
@@ -0,0 +1,8 @@
+package com.meyersj.mobilesurveyor.app;
+
+
+public class Fields {
+    public static final String LAT = "lat";
+    public static final String LON = "lon";
+    public static final String SECONDS = "seconds";
+}
