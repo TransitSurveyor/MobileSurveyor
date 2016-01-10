68244b872ea01eb99da1104c6f1cdf4e4ffb1f5e
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-09 17:19:10 -0800

    Simplify Application class

--- app/src/main/java/com/meyersj/mobilesurveyor/app/TransitSurveyorApplication.java
+++ app/src/main/java/com/meyersj/mobilesurveyor/app/TransitSurveyorApplication.java
@@ -1,23 +1,11 @@
 package com.meyersj.mobilesurveyor.app;
 
 import android.app.Application;
-import android.util.Log;
-
-import com.meyersj.mobilesurveyor.app.util.Cons;
-import com.meyersj.mobilesurveyor.app.util.Utils;
-
-import java.util.Properties;
 
 public class TransitSurveyorApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Properties prop = Utils.getProperties(this, "config.properties");
-        String token = prop.getProperty(Cons.NEWRELIC, "");
-        Log.d("SurveyApplication", "token: " + token);
-        //if (!token.isEmpty()) {
-        //    NewRelic.withApplicationToken(token).start(this);
-        //}
     }
 }
