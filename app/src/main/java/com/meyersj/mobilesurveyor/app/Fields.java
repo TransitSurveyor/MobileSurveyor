74eda0374e263f6389d48247af840305b8d5118d
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-11 17:50:41 -0800

    add `rte` and `dir` to Fields constants

--- app/src/main/java/com/meyersj/mobilesurveyor/app/Fields.java
+++ app/src/main/java/com/meyersj/mobilesurveyor/app/Fields.java
@@ -2,6 +2,8 @@ package com.meyersj.mobilesurveyor.app;
 
 
 public class Fields {
+    public static final String ROUTE = "rte";
+    public static final String DIRECTION = "dir";
     public static final String LAT = "lat";
     public static final String LON = "lon";
     public static final String SECONDS = "seconds";
